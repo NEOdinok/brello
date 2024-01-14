2b3486fcc7a3a85fb4a7de2cdb7b75446796205d
goshamartynovich <goshamartynovich@gmail.com>
2024-01-14 19:30:56 +0300

    feat: add SVGR plugin, css modules section to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "node:path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+const MODE = {
+  DEV: "development",
+  PROD: "production",
+};
+
+export default defineConfig(({ mode }) => {
+  const generateScopedName =
+    mode === MODE.DEV
+      ? "[folder]--[local]-[hash:base64:5]"
+      : "[local]-[hash:base64:5]";
+
+  return {
+    plugins: [react({ babel: { babelrc: true } }), svgr()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    css: {
+      modules: {
+        generateScopedName,
+        localsConvention: "camelCase",
+      },
+    },
+  };
 });
