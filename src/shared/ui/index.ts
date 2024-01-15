d8085700a50b20a82ccd01f18dedc92bbd083914
goshamartynovich <goshamartynovich@gmail.com>
2024-01-15 18:05:16 +0300

    chore: export shared ui components

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./button";
+export { FeaturedIcon } from "./featured-icon";
+export { Input } from "./input";
+export { Loading } from "./loading";
+export { Logo } from "./logo";
