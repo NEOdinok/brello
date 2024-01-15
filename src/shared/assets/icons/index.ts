ce119dcf659f6fa50fcecdcf8d87fa57d4ccac05
goshamartynovich <goshamartynovich@gmail.com>
2024-01-15 17:44:27 +0300

    feat: add icons and images for sign-in page

---
+++ src/shared/assets/icons/index.ts
@@ -0,0 +1,3 @@
+export { ReactComponent as IconMail01 } from "./mail-01.svg";
+export { ReactComponent as IconAlertCircle } from "./alert-circle.svg";
+export { ReactComponent as IconArrowLeft } from "./arrow-left.svg";
