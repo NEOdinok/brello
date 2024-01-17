7192bc2b7dce486279884f3cb4b1341429230c8c
goshamartynovich <goshamartynovich@gmail.com>
2024-01-17 18:55:43 +0300

    fix: onboarding route name

--- src/pages/onboarding/index.ts
+++ src/pages/onboarding/index.ts
@@ -1,7 +1,7 @@
 import { currentRoute } from "./model";
 import { OnboardingPage } from "./page";
 
-export const HomeRoute = {
+export const OnboardingRoute = {
   view: OnboardingPage,
   route: currentRoute,
 };
