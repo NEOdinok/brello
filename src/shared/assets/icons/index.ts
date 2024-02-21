e853855316723696d34feb6e08919e704184a034
goshamartynovich <goshamartynovich@gmail.com>
2024-02-21 16:58:56 +0300

    chore: assets for user onboarding page

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as IconMail01 } from "./mail-01.svg";
 export { ReactComponent as IconAlertCircle } from "./alert-circle.svg";
 export { ReactComponent as IconArrowLeft } from "./arrow-left.svg";
+export { ReactComponent as IconUser01 } from "./user-01.svg";
