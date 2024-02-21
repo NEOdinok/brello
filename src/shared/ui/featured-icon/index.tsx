c878cc2de9cbd2bd6c47179dd0c872873b4b7a7e
goshamartynovich <goshamartynovich@gmail.com>
2024-02-21 16:55:21 +0300

    feat: updated featured icon

--- src/shared/ui/featured-icon/index.tsx
+++ src/shared/ui/featured-icon/index.tsx
@@ -6,11 +6,17 @@ import styles from "./styles.module.css";
 interface Props {
   className?: string;
   color?: "primary" | "error";
+  theme?: "light-circle-outline" | "modern";
   Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
 }
 
-export const FeaturedIcon: FC<Props> = ({ className, color = "primary", Icon }) => {
-  const classList = cn(styles.root, styles[`color-${color}`], className);
+export const FeaturedIcon: FC<Props> = ({
+  className,
+  color = "primary",
+  Icon,
+  theme = "light-circle-outline",
+}) => {
+  const classList = cn(styles.root, styles[`color-${color}`], styles[`theme-${theme}`], className);
 
   return (
     <div className={classList}>
