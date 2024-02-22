5aa7cb04250018abacbdb3750c7c39d1538aee21
goshamartynovich <goshamartynovich@gmail.com>
2024-02-23 00:21:37 +0300

    feat: + workspace entity

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -3,6 +3,7 @@ import * as profiles from "./rest/profiles";
 import * as workspaces from "./rest/workspaces";
 
 export type { User } from "./rest/common";
+export type { Workspace } from "./rest/workspaces";
 
 export const api = {
   auth,
