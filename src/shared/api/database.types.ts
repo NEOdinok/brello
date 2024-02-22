e8b2bb2c79f5e90fae0a73c4d857d71065934cf2
goshamartynovich <goshamartynovich@gmail.com>
2024-02-22 14:07:14 +0300

    chore: database types

--- src/shared/api/database.types.ts
+++ src/shared/api/database.types.ts
@@ -1,6 +1,6 @@
 export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       profiles: {
@@ -29,6 +29,45 @@ export interface Database {
           {
             foreignKeyName: "profiles_id_fkey";
             columns: ["id"];
+            isOneToOne: true;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          },
+        ];
+      };
+      workspaces: {
+        Row: {
+          avatar_url: string | null;
+          created_at: string;
+          description: string | null;
+          id: string;
+          name: string;
+          slug: string | null;
+          user_id: string;
+        };
+        Insert: {
+          avatar_url?: string | null;
+          created_at?: string;
+          description?: string | null;
+          id?: string;
+          name: string;
+          slug?: string | null;
+          user_id: string;
+        };
+        Update: {
+          avatar_url?: string | null;
+          created_at?: string;
+          description?: string | null;
+          id?: string;
+          name?: string;
+          slug?: string | null;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "public_workspaces_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
             referencedRelation: "users";
             referencedColumns: ["id"];
           },
@@ -48,4 +87,4 @@ export interface Database {
       [_ in never]: never;
     };
   };
-}
+};
