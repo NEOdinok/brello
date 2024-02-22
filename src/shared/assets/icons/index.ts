ef6368c192f59b9e14f89e3c60df3e4b5bcb8eee
goshamartynovich <goshamartynovich@gmail.com>
2024-02-22 19:01:33 +0300

    feat: assets for onboarding page

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as IconMail01 } from "./mail-01.svg";
 export { ReactComponent as IconAlertCircle } from "./alert-circle.svg";
 export { ReactComponent as IconArrowLeft } from "./arrow-left.svg";
 export { ReactComponent as IconUser01 } from "./user-01.svg";
+export { ReactComponent as IconFolderShield } from "./folder-shield.svg";
