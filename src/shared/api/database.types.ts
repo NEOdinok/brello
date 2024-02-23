ba7cfa742feec826a42d546e327dd526c3b9e93d
goshamartynovich <goshamartynovich@gmail.com>
2024-02-23 12:17:20 +0300

    chore: database types fixed

--- src/shared/api/database.types.ts
+++ src/shared/api/database.types.ts
@@ -1,35 +1,40 @@
 export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
-export type Database = {
+export interface Database {
   public: {
     Tables: {
       profiles: {
         Row: {
           avatar_url: string | null;
-          first_name: string | null;
+          created_at: string;
+          first_name: string;
           id: string;
           last_name: string | null;
-          updated_at: string | null;
+          updated_at: string;
+          user_id: string;
         };
         Insert: {
           avatar_url?: string | null;
-          first_name?: string | null;
-          id: string;
+          created_at?: string;
+          first_name: string;
+          id?: string;
           last_name?: string | null;
-          updated_at?: string | null;
+          updated_at?: string;
+          user_id: string;
         };
         Update: {
           avatar_url?: string | null;
-          first_name?: string | null;
+          created_at?: string;
+          first_name?: string;
           id?: string;
           last_name?: string | null;
-          updated_at?: string | null;
+          updated_at?: string;
+          user_id?: string;
         };
         Relationships: [
           {
-            foreignKeyName: "profiles_id_fkey";
-            columns: ["id"];
-            isOneToOne: true;
+            foreignKeyName: "profiles_user_id_fkey";
+            columns: ["user_id"];
             referencedRelation: "users";
             referencedColumns: ["id"];
           },
@@ -65,9 +70,8 @@ export type Database = {
         };
         Relationships: [
           {
-            foreignKeyName: "public_workspaces_user_id_fkey";
+            foreignKeyName: "workspaces_user_id_fkey";
             columns: ["user_id"];
-            isOneToOne: false;
             referencedRelation: "users";
             referencedColumns: ["id"];
           },
@@ -87,4 +91,4 @@ export type Database = {
       [_ in never]: never;
     };
   };
-};
+}
