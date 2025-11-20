7cb150b7588394d18b3b067ae330a83f92e66b3f
goshamartynovich <goshamartynovich@gmail.com>
2025-11-20 22:02:34 +0300

    removed test counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,11 @@ import "@mantine/core/styles/ActionIcon.css";
 import styles from "./app.module.css";
 import { Header } from "./header";
 import { KanbanBoard } from "./kanban";
-import { $counter, incrementClicked } from "./model";
-import { Button } from "./button";
-import { useUnit } from "effector-react";
 
 export default function App() {
-  const Counter = () => {
-    const [counter, handleIncrement] = useUnit([$counter, incrementClicked]);
-
-    return <Button onClick={handleIncrement}>{counter}</Button>;
-  };
-
   return (
     <>
       <Header />
-      <Counter />
       <main className={styles.main}>
         <KanbanBoard />
       </main>
